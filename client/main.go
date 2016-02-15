3fe3dc04d2efe81c03c04d9a35fd32527d207c55
David Tomberlin <siyegen@gmail.com>
2016-02-14 18:33:28 -0800

    some improvements + sound

--- client/main.go
+++ client/main.go
@@ -7,19 +7,32 @@ import (
 )
 
 const POM_TIME time.Duration = time.Minute * 25
-const cmd string = "osascript"
 
-var argsAA = []string{"-e", "display notification \"moo\" with title \"MyTitle\""}
+const OSX_CMD string = "osascript"
+
+type OSXNotifier struct {
+	baseCommand string
+	flag        string
+	// messageArg  string
+}
+
+func (n *OSXNotifier) sendNotification(message, title string) error {
+	fullMessage := fmt.Sprintf("display notification \"%s\" with title \"%s\"", message, title)
+	cmdArgs := []string{n.flag, fullMessage}
+	_, err := exec.Command(OSX_CMD, cmdArgs...).Output()
+	return err
+}
 
 func main() {
 	fmt.Println("Starting a pom!")
+	nnn := &OSXNotifier{OSX_CMD, "-e"}
 	// pomTimer := time.NewTimer(POM_TIME)
 	// <-pomTimer.C
-	out, err := exec.Command(cmd, argsAA...).Output()
+	// out, err := exec.Command(cmd, argsAA...).Output()
+	err := nnn.sendNotification("I'm mister message", "Title bitch")
 	if err != nil {
 		fmt.Println("Error", err)
-	} else {
-		fmt.Println("Output", string(out))
 	}
+
 	fmt.Println("Finished a pom!")
 }
--- server/server.go
+++ server/server.go
@@ -9,59 +9,84 @@ import (
 	"github.com/gorilla/mux"
 )
 
-const POM_TIME time.Duration = time.Minute * 25
+const POM_TIME time.Duration = time.Second * 15
 const OSX_CMD string = "osascript"
 
+type App struct {
+	runningPom bool
+	results    map[string]int
+	notifier   *OSXNotifier
+}
+
+func (app *App) apiStartHandler(res http.ResponseWriter, req *http.Request) {
+	if !app.runningPom {
+		fmt.Println("Starting Pom")
+		app.runningPom = true
+		pomTimer := time.NewTimer(POM_TIME)
+		go func() {
+			<-pomTimer.C
+			fmt.Println("Finished POM")
+			app.results["Success"] = app.results["Success"] + 1
+			app.runningPom = false
+			err := app.notifier.sendNotification("Pom finished", "Oswald")
+			if err != nil {
+				fmt.Println("Failed to send notifications", err)
+			}
+		}()
+		res.WriteHeader(http.StatusCreated)
+		res.Write([]byte(fmt.Sprintf("Started POM at %s", time.Now())))
+	} else {
+		res.WriteHeader(http.StatusBadRequest)
+		res.Write([]byte("Pom already running, pause or cancel first"))
+	}
+}
+
+func (app *App) apiStatusHandler(res http.ResponseWriter, req *http.Request) {
+	if app.runningPom {
+		res.WriteHeader(http.StatusConflict)
+		res.Write([]byte("Currently in pom, %time left"))
+	} else {
+		success := app.results["Success"]
+		cancelled := app.results["Cancelled"]
+		paused := app.results["Paused"]
+		res.WriteHeader(http.StatusOK)
+		res.Write([]byte(fmt.Sprintf("Success: %d, Cancelled: %d, Paused: %d", success, cancelled, paused)))
+	}
+}
+
+type OSXNotifier struct {
+	baseCommand string
+	flag        string
+}
+
+func (n *OSXNotifier) sendNotification(message, title string) error {
+	fullMessage := fmt.Sprintf("display notification \"%s\" with title \"%s\" sound name \"Submarine\"", message, title)
+	cmdArgs := []string{n.flag, fullMessage}
+	_, err := exec.Command(n.baseCommand, cmdArgs...).Output()
+	return err
+}
+
 func main() {
-	cmdArgs := []string{"-e", "display notification \"Finished POM!\" with title \"Oswald:\""}
-	results := map[string]int{"Success": 0, "Cancelled": 0, "Paused": 0}
-	portString := ":13380"
-	runningPom := false
-	fmt.Println("Starting Server at:", portString)
-	// Start a server
+	osxNotifier := &OSXNotifier{OSX_CMD, "-e"}
+
+	app := &App{
+		runningPom: false,
+		notifier:   osxNotifier,
+		results:    map[string]int{"Success": 0, "Cancelled": 0, "Paused": 0},
+	}
+
+	portString := ":13381"
 	r := mux.NewRouter()
 
 	// api endpoints
-	r.HandleFunc("/start", func(res http.ResponseWriter, req *http.Request) {
-		if !runningPom {
-			fmt.Println("Starting Pom")
-			runningPom = true
-			pomTimer := time.NewTimer(POM_TIME)
-			go func() {
-				<-pomTimer.C
-				fmt.Println("Finished POM")
-				results["Success"] = results["Success"] + 1
-				runningPom = false
-				_, err := exec.Command(OSX_CMD, cmdArgs...).Output()
-				if err != nil {
-					fmt.Println("Failed to send notifications", err)
-				}
-			}()
-			res.WriteHeader(http.StatusCreated)
-			res.Write([]byte(fmt.Sprintf("Started POM at %s", time.Now())))
-		} else {
-			res.WriteHeader(http.StatusBadRequest)
-			res.Write([]byte("Pom already running, pause or cancel first"))
-		}
-	})
+	r.HandleFunc("/start", app.apiStartHandler)
+	r.HandleFunc("/status", app.apiStatusHandler)
 
 	r.HandleFunc("/stop", func(res http.ResponseWriter, req *http.Request) {
 		fmt.Println("stop Pom")
 	})
 
-	r.HandleFunc("/status", func(res http.ResponseWriter, req *http.Request) {
-		if runningPom {
-			res.WriteHeader(http.StatusConflict)
-			res.Write([]byte("Currently in pom, %time left"))
-		} else {
-			success := results["Success"]
-			cancelled := results["Cancelled"]
-			paused := results["Paused"]
-			res.WriteHeader(http.StatusOK)
-			res.Write([]byte(fmt.Sprintf("Success: %d, Cancelled: %d, Paused: %d", success, cancelled, paused)))
-		}
-	})
-
+	fmt.Println("Starting Server at:", portString)
 	http.ListenAndServe(portString, r)
 	fmt.Println("Shutting down")
 }
