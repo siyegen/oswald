d89b5b3c22eb9f291dbcb189e4908b0be12c4917
David Tomberlin <siyegen@gmail.com>
2016-02-19 11:20:21 -0800

    experiment with a reminder

--- other/other.go
+++ other/other.go
@@ -1,1 +1,18 @@
 package other
+
+import "time"
+
+type PomReminder struct {
+	title    string
+	message  string
+	remindIn time.Duration
+
+	timer *time.Timer
+}
+
+func (p *PomReminder) setReminder() {
+	p.timer = time.NewTimer(p.remindIn)
+	go func() {
+		<-p.timer.C
+	}()
+}
